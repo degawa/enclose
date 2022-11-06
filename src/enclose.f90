98428a7e8dc4f927ed011bb6e481918202171671
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-11-06 11:52:52 +0900

    added `enclose_autoclose`

--- src/enclose.f90
+++ src/enclose.f90
@@ -6,7 +6,16 @@ module strings_enclose
 
     interface enclose
         procedure :: enclose_open_close
+        procedure :: enclose_autoclose
     end interface
+
+    character(*), private, parameter :: bracket_open =  "(<[{)>]}" !&
+        !! opening symbols
+    character(*), private, parameter :: bracket_close = ")>]}(<[{"
+        !! closing symbols paired with the opening ones
+
+    logical, private, parameter :: autoclose_default_value = .true.
+        !! the default value of the optional argument `autoclose`
 
 contains
     !>Returns string enclosed by `open` and `close` symbols.
@@ -31,4 +40,63 @@ contains
         str_enclosed = open//str//close
     end function enclose_open_close
 
+    function enclose_autoclose(str, bracket, autoclose) result(str_enclosed)
+        implicit none
+        !&<
+        character(*), intent(in)            :: str
+            !! A string to be enclosed
+        character(*), intent(in)            :: bracket
+            !! Opening bracket symbols
+        logical     , intent(in), optional  :: autoclose
+            !! Automatically determining closing symbols
+            !! as the pair of opening symbols
+        !&>
+        character(:), allocatable :: str_enclosed
+            !! An string enclosed by opening and closing symbols
+
+        logical :: autoclosing
+
+        autoclosing = autoclose_default_value
+        if (present(autoclose)) autoclosing = autoclose
+
+        block
+            if (.not. autoclosing) then
+                str_enclosed = enclose_open_close(str, bracket, bracket)
+                return
+            end if
+        end block
+
+        block
+            character(:), allocatable :: close
+                !! closing symbols
+            integer(int32) :: pos
+                !! position (array index) of a opening bracket
+                !! in `bracket_open`
+
+            close = ""
+
+            do pos = len(bracket), 1, -1
+                close = close//get_closing_symbol(bracket(pos:pos))
+            end do
+
+            str_enclosed = enclose_open_close(str, bracket, close)
+            return
+        end block
+    contains
+        function get_closing_symbol(char) result(close)
+            implicit none
+            character, intent(in) :: char
+                !! An opening symbol
+            character :: close
+                !! A closing symbol
+
+            integer(int32) :: pos
+            pos = index(bracket_open, char)
+            if (pos >= 1) then ! if `substring` is not in `string`, `index(string, substring)` returns 0
+                close = bracket_close(pos:pos)
+            else
+                close = char
+            end if
+        end function get_closing_symbol
+    end function enclose_autoclose
 end module strings_enclose
