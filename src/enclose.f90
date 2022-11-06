7c8250a13ff312762ca2e4010226e6eba6333524
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2022-11-06 11:08:45 +0900

    added `enclose_open_close` procedure

---
+++ src/enclose.f90
@@ -0,0 +1,28 @@
+module strings_enclose
+    use, intrinsic :: iso_fortran_env
+    implicit none
+    private
+    public :: enclose
+
+    interface enclose
+        procedure :: enclose_open_close
+    end interface
+
+contains
+    function enclose_open_close(str, open, close) result(str_enclosed)
+        implicit none
+        !&<
+        character(*), intent(in) :: str
+            !! A string to be enclosed
+        character(*), intent(in) :: open
+            !! Opening bracket symbols
+        character(*), intent(in) :: close
+            !! Closing bracket symbols
+        !&>
+        character(:), allocatable :: str_enclosed
+            !! An string enclosed by opening and closing symbols
+
+        str_enclosed = open//str//close
+    end function enclose_open_close
+
+end module strings_enclose
