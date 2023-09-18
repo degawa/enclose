fd2638794a2bf220258250575934ca25306872e0
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-09-19 00:24:45 +0900

    removed stdlib from dev-dependencies

--- example/demo.f90
+++ example/demo.f90
@@ -1,8 +1,6 @@
 program demo
     use, intrinsic :: iso_fortran_env
     use :: strings_enclose
-    use :: stdlib_logger
-    use :: stdlib_strings
     implicit none
 
     real(real64), allocatable :: u(:, :)
@@ -12,24 +10,33 @@ program demo
 
     block
         variable_name = "`u`"
-        call global_logger%log_information(variable_name//" allocation status "//to_string(allocated(u)))
-        call global_logger%log_information(variable_name//" range [["// &
-                                           to_string(lbound(u, 1))//", "// &
-                                           to_string(ubound(u, 1))//"], ["// &
-                                           to_string(lbound(u, 2))//", "// &
-                                           to_string(ubound(u, 2))//"]]")
+        print *, variable_name//" range [["// &
+            to_string(lbound(u, 1))//", "// &
+            to_string(ubound(u, 1))//"], ["// &
+            to_string(lbound(u, 2))//", "// &
+            to_string(ubound(u, 2))//"]]"
     end block
 
     block
         variable_name = enclose("u", "`")
-        call global_logger%log_information(variable_name//" allocation status "//to_string(allocated(u)))
-        call global_logger%log_information(variable_name//" range "// &
-                                           enclose( &
-                                           enclose(to_string(lbound(u, 1))//", "// &
-                                                   to_string(ubound(u, 1)), "[") &
-                                           //", "// &
-                                           enclose(to_string(lbound(u, 2))//", "// &
-                                                   to_string(ubound(u, 2)), "["), &
-                                           "["))
+        print *, variable_name//" range "// &
+            enclose( &
+            enclose(to_string(lbound(u, 1))//", "// &
+                    to_string(ubound(u, 1)), "[") &
+            //", "// &
+            enclose(to_string(lbound(u, 2))//", "// &
+                    to_string(ubound(u, 2)), "["), &
+            "[")
     end block
+
+contains
+    function to_string(i) result(str)
+        implicit none
+        integer(int32), intent(in) :: i
+        character(:), allocatable :: str
+
+        character(14) :: buf
+        write (buf, '(I0)') i
+        str = trim(buf)
+    end function to_string
 end program demo
